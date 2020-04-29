327123549153bd26ab3601fb93142dd60eee7def
Si Choi <sichoi@Siui-MacBook-Pro.local>
2020-04-29 22:36:16 +0900

    first commit

---
+++ build/precache-manifest.211f67a2aaeffcf79427f7d2b27b7c19.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3d43e4432d135bd1ce6911dcf9b4129",
+    "url": "/index.html"
+  },
+  {
+    "revision": "506baefd3cc4de1c7c2f",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "d0db0022ded433a89172",
+    "url": "/static/css/main.352c5307.chunk.css"
+  },
+  {
+    "revision": "506baefd3cc4de1c7c2f",
+    "url": "/static/js/2.be7cb959.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.be7cb959.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0db0022ded433a89172",
+    "url": "/static/js/main.4b23766a.chunk.js"
+  },
+  {
+    "revision": "19133a5d6c7be98533aa",
+    "url": "/static/js/runtime-main.107bb8ca.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
